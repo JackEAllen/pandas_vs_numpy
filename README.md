6324f665062330847659ea545fd648824b6ac494
JackEAllen <mr.jack.e.allen@gmail.com>
2020-03-26 12:00:12 +0000

    Creation of example script to display ideal plotting format and updated README.md

--- README.md
+++ README.md
@@ -17,8 +17,48 @@ Running locally will display two visualisations which can also be seen in the im
 
 The top line plot is GEM spectrum data while the second line plot is of randomly generate data. 
 
-When plotting data using Pandas dataframes, no additional modifications are requered. Optionally for readability, you can add labels to call columns by name as to numerical index.
+When plotting data using Pandas dataframes, no additional modifications are requered. Optionally 
+for readability, you can add labels to call columns by name as to numerical index.
 
-Plotting using Numpy arrays requires the data to be transposed to rotate the data in order to visualise due to how Numpy arrays are read. You are unable to add labels to these arrays meaning they are less readable. A requirment to understand the data would be needed if Numpy arrays are used.
+Plotting using Numpy arrays requires the data to be transposed to rotate the data in order to 
+visualise due to how Numpy arrays are read. You are unable to add labels to these arrays meaning they are less readable. A requirment to understand the data would be needed if Numpy arrays are used.
 
-A possible solution to this would be to place each sub-array inside the numpy array inside a dictionary to use the key as a labels for reference.
+A possible solution to this would be to place each sub-array inside the numpy array inside a 
+dictionary to use the key as a labels for reference.
+
+## Running plotting_multi_spectra_data.py
+
+This script is stored inside the plotting_format_example directory
+
+The dataframe containing raw data follows the below structure:
+
+```
+|Spectrum | X      | Y   | E  |
+| :---    | :---: |:---:| ---:|
+| 1.0     |       |     |     |
+| 1.0     | 1     | 2   | 3   |
+| 1.0     | 4     | 5   | 6   |
+| 2.0     | 1     | 2   | 3   |
+| 2.0     | 4     | 5   | 6   |
+```
+
+Please note that this script takes some time to run due to the large size of the raw data being 
+plotted. 
+
+Only when you receive an output in your terminal similar to below is the visualisation ready. 
+
+```
+Running on http://127.0.0.1:8050/
+Debugger PIN: 880-009-103
+ * Serving Flask app "plotting_with_raw_multi_spectra_data" (lazy loading)
+ * Environment: production
+   WARNING: This is a development server. Do not use it in a production deployment.
+   Use a production WSGI server instead.
+ * Debug mode: on
+Running on http://127.0.0.1:8050/
+Debugger PIN: 477-245-086
+```
+
+The output in the web-page should look similar to this:
+
+![](raw_data_visaulualisation.png)
---
+++ pandas_vs_numpy/plotting.py
@@ -0,0 +1,116 @@
+# Dependencies
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+import plotly.graph_objs as go
+import pandas as pd
+import os
+from numpy import genfromtxt
+
+
+gem_df = pd.read_csv('GEM-Spectrum1.csv')
+gem_df.columns = ['X', 'Y', 'E']
+print(gem_df)
+
+mod_df = gem_df*2
+
+# print(mod_df)
+
+# gem_df.append(mod_df, ignore_index=True)
+
+gem_df.to_csv('data_frame.csv', index=False)
+
+# print(gem_df)
+# print(gem_df[gem_df.isna().any(axis=1)])
+
+
+# Rotate and + 1
+gem_np = gem_df.to_numpy().T+1
+
+# Basic test data for plotting
+column_names = ['month', 'high_2000', 'low_2000', 'high_2007', 'low_2007', 'high_2014', 'low_2014']
+
+# pandas dataframe
+df = pd.DataFrame([['January', 'February', 'March', 'April', 'May', 'June', 'July',
+                    'August', 'September', 'October', 'November', 'December'],
+                   [32.5, 37.6, 49.9, 53.0, 69.1, 75.4, 76.5, 76.6, 70.7, 60.6, 45.1, 29.3],
+                   [13.8, 22.3, 32.5, 37.2, 49.9, 56.1, 57.7, 58.3, 51.2, 42.8, 31.6, 15.9],
+                   [36.5, 26.6, 43.6, 52.3, 71.5, 81.4, 80.5, 82.2, 76.0, 67.3, 46.1, 35.0],
+                   [23.6, 14.0, 27.0, 36.8, 47.6, 57.7, 58.9, 61.2, 53.3, 48.5, 31.0, 23.6],
+                   [28.8, 28.5, 37.0, 56.8, 69.7, 79.7, 78.5, 77.8, 74.1, 62.6, 45.3, 39.9],
+                   [12.7, 14.3, 18.6, 35.5, 49.9, 58.0, 60.0, 58.6, 51.7, 45.2, 32.2, 29.1]])
+
+# numpy arrayGEM-Spectrum1.csv
+np_array = df.to_numpy()
+
+# Rotate to assign columns names to dataframe - this a separate step to allow for easier conversion
+# of pandas dataframe into numpy array as such an array could not have columns
+df = df.T
+
+df.columns = column_names
+
+# Using pandas dataframes
+x_axis = "month"
+y_axis = "high_2014"
+
+# Using pandas dataframe
+trace1 = go.Scatter(x=df[x_axis],
+                    y=df[y_axis],
+                    name="Pandas",
+                    opacity=0.7)
+
+# Using numpy array
+trace2 = go.Scatter(x=np_array[0],  # month
+                    y=np_array[2],  # low_2000
+                    name="Numpy",
+                    opacity=0.7)
+# Using pandas dataframe
+trace3 = go.Scatter(x=gem_df['X'],
+                    y=gem_df['Y'],
+                    name="Pandas",
+                    opacity=0.7)
+
+# Using numpy array
+trace4 = go.Scatter(x=gem_np[0],
+                    y=gem_np[1],
+                    name="Numpy",
+                    opacity=0.7)
+
+# data to populate plots with                   
+data = [trace3, trace4]
+data2 = [trace1, trace2]
+
+layout = dict(title=os.path.basename(__file__).split('.')[0])
+
+# Figure creation 
+fig = dict(data=data,
+           layout=layout)
+
+fig2 = dict(data=data2,
+            layout=layout)
+
+
+# Dash app Initialization
+app = dash.Dash()
+
+
+# Dash app structure
+app.layout = html.Div([
+    # Plot one
+    html.Div(
+        dcc.Graph(
+            id='ID',
+            figure=fig
+        )
+    ),
+    # Plot two
+    html.Div(
+        dcc.Graph(
+            id='ID2',
+            figure=fig2
+        )
+    )
+])
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
